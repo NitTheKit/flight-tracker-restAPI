1db59e50e9891a21eb598c2e65912f7ec7806c59
Nit-Brown <nitin_sreekumar@brown.edu>
2022-11-04 16:40:13 -0400

    Table being modified

---
+++ Client/app.js
@@ -0,0 +1,23 @@
+
+const tableBody = document.getElementById('table-body')
+
+const getFlight = () => {
+    fetch('http://localhost:8000/flights')
+    .then(response => response.json())
+    .then(flights => {
+       populateTable(flights)
+    })
+    .catch(err => console.log(err))
+}
+
+getFlight()
+
+const populateTable = (flights) => {
+    for (const flight of flights){
+        const tableRow = document.createElement('tr')
+        const tableIcon = document.createElement('td')
+        tableIcon.textContent = "✈"
+        tableRow.append(tableIcon)
+        tableBody.append(tableRow)
+    } 
+}
